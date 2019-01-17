89c5df498b170c42d1f5084fdac8dc482392cfa6
Sean G <greseam@dunwoody.edu>
2019-01-17 14:08:12 -0600

    quality of life changes

--- lab_one/Program.cs
+++ lab_one/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlTypes;
 using System.Linq;
 
 namespace lab_one
@@ -36,19 +37,33 @@ namespace lab_one
             string[] questionNum = {"1. ","2. ","3. ","4. ","5. ","6. ","7. ","8. ","9. ","10. "}; // use this as guide for the program
             List<int> Scores = new List<int>();
 
-            Console.WriteLine("\tWelcome, this program will assess your knowledge of .NET Core");
-            Console.WriteLine("To get started, please enter a number other than -1 to start program");
-            string stringStart = Console.ReadLine();
             int attempt = 2;
             int maxAttempt = 0;
             int Exit = -1;
+            int broke = -1;
             int Score = 0;
             const int maxScore = 10;
             int Error = 0;
-            int Start = Convert.ToInt16(stringStart);
             List<string> correct = new List<string>();
             List<string> wrong = new List<string>();
-            Console.Clear();
+            int Start = -2;
+
+            while (Start == -2) //begining of Console app
+            {
+                Console.WriteLine("\tWelcome, this program will assess your knowledge of .NET Core");
+                Console.WriteLine("To get started, please press the SACE key to start program");
+                ConsoleKey Input = Console.ReadKey().Key;
+                if (Input == ConsoleKey.Spacebar)
+                {
+                    Start = 0;
+                    Console.Clear();
+                    play();
+                }
+                else
+                {
+                    Console.Clear();
+                }
+            }
 
             void ScoreBoard()
             {
@@ -67,7 +82,7 @@ namespace lab_one
                     else if (Error > 0)
                     {
                         Console.ForegroundColor = ConsoleColor.Yellow;
-                        Console.WriteLine("\tYou have not anwsered all the questions for this attempt!\n");
+                        Console.WriteLine("\tYou did not anwser all the questions for this attempt!\n");
                         Console.ForegroundColor = ConsoleColor.White;
                     }
                     else
@@ -78,89 +93,112 @@ namespace lab_one
                     }
                 }
 
-                Console.WriteLine("\nThank you for taking this small quiz");
-            }
-
-            while (Start != Exit)
-            {
-                for (int i = 0; i < 10; i++)
-                {
-                    Console.WriteLine(questionNum[i] + questions[i]);
-                    for (int j = 0; j < quizOptions.GetLength(1); j++)
-                    {
-                        Console.WriteLine(quizOptions[i,j]);
-                    }
-
-                    Console.WriteLine("Please enter an option");
-                    string userSelection= Console.ReadLine();
-                    Console.Clear();
-
-                    //need to determine if the users selection is the correct answer 
-                    if (userSelection == cheatSheet[i])
-                    {
-                        Console.ForegroundColor = ConsoleColor.Green;
-                        correct.Add(userSelection);
-                        Console.WriteLine("Correct");
-                        Console.ForegroundColor = ConsoleColor.White;
-                        Console.WriteLine("Press Enter to continue...");
-                        Console.ReadKey();
-                        Console.Clear();
-                        Score++;
-                    }
-                    else if (userSelection == "")
-                    {
-                        Console.ForegroundColor = ConsoleColor.Yellow;
-                        correct.Add(userSelection);
-                        Console.WriteLine("Error, no option given!");
-                        Console.ForegroundColor = ConsoleColor.White;
-                        Console.WriteLine("Press Enter to continue...");
-                        Console.ReadKey();
-                        Console.Clear();
-                        Error++;
-                    }
-                    else
-                    {
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        wrong.Add(userSelection);
-                        Console.WriteLine("Incorect");
-                        Console.ForegroundColor = ConsoleColor.White;
-                        Console.WriteLine("Press Enter to continue...");
-                        Console.ReadKey();
-                        Console.Clear();
-                        
-                    }
-                    
-                }
-                Scores.Add(Score);
-                Score = 0;
-                Console.WriteLine("Would you like to attempt again? You can attempt " + Convert.ToString(attempt) + " more times!\nHitting N will Exit and show your scores and attempts (Y/N)");
-                string tryAgain = Console.ReadLine();
-                maxAttempt++;
+                Console.WriteLine("\nThank you for taking this small quiz\n\tHave a great day!!!\n");
+                Start = -1;
                 
 
-                if (tryAgain == "N")
+            }
+            
+            void play() //begining of quiz
+            {
+                while (Start != Exit)
                 {
-                    //exit
-                    Start = -1; 
-                    ScoreBoard();
-                }
-                else
-                {
-                    Console.Clear();
-                    attempt--;
-                    if (attempt < 0)
+                    for (int i = 0; i < 10; i++)
                     {
-                        Console.WriteLine("You are out of attempts");
-                        Console.WriteLine("Press Enter to continue...");
-                        Console.ReadKey();
+                        Console.WriteLine(questionNum[i] + questions[i]);
+                        for (int j = 0; j < quizOptions.GetLength(1); j++)
+                        {
+                            Console.WriteLine(quizOptions[i, j]);
+                        }
+
+                        Console.WriteLine("Please enter an option");
+                        string userSelection = Console.ReadLine();
                         Console.Clear();
-                        ScoreBoard();
-                        Start = -1;
+
+                        //need to determine if the users selection is the correct answer 
+                        if (userSelection == cheatSheet[i])
+                        {
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            correct.Add(userSelection);
+                            Console.WriteLine("Correct");
+                            Console.ForegroundColor = ConsoleColor.White;
+                            Console.WriteLine("Press Enter to continue...");
+                            Console.ReadKey();
+                            Console.Clear();
+                            Score++;
+                        }
+                        else if (userSelection == "")
+                        {
+                            Console.ForegroundColor = ConsoleColor.Yellow;
+                            correct.Add(userSelection);
+                            Console.WriteLine("Error, no option given!");
+                            Console.ForegroundColor = ConsoleColor.White;
+                            Console.WriteLine("Press Enter to continue...");
+                            Console.ReadKey();
+                            Console.Clear();
+                            Error++;
+                        }
+                        else
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            wrong.Add(userSelection);
+                            Console.WriteLine("Incorect");
+                            Console.ForegroundColor = ConsoleColor.White;
+                            Console.WriteLine("Press Enter to continue...");
+                            Console.ReadKey();
+                            Console.Clear();
+
+                        }
+
                     }
-                    //play again
+
+                    Scores.Add(Score);
+                    Score = 0;
+                    Console.WriteLine("Would you like to attempt again? You can attempt " + Convert.ToString(attempt) +
+                                      " more times!\nHitting N will Exit and show your scores and attempts (Y/N)");
+
+                    maxAttempt++;
+                    while (broke == -1) //Change this to work with three attempts
+                    {
+                        ConsoleKey tryAgain = Console.ReadKey().Key;
+                        if (tryAgain == ConsoleKey.N)
+                        {
+                            Console.Clear();
+                            Start = -1;
+                            broke = 1;
+                            ScoreBoard();
+                        }
+                        else if (tryAgain == ConsoleKey.Y)
+                        {
+                            Console.Clear();
+                            attempt--;
+                            if (attempt < 0)
+                            {
+                                Console.WriteLine("You are out of attempts");
+                                Console.WriteLine("Press Enter to continue...");
+                                Console.ReadKey();
+                                broke = 1;
+                                Start = -1;
+                                Console.Clear();
+                                ScoreBoard();
+                            }
+                            play();
+                        }
+                        else
+                        {
+                            if (attempt < 0)
+                            {
+                                Console.ReadKey();
+                                Console.Clear();
+                                Start = -1;
+                            }
+                        }
+                        
+                    }
+
+                    
                 }
             }
-
         }
     }
 }
