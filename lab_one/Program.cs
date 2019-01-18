ae2112390384ff4d2572eb3a8b36b2317bdd4f38
Sean G <greseam@dunwoody.edu>
2019-01-18 11:55:45 -0600

    Add questions

--- lab_one/Program.cs
+++ lab_one/Program.cs
@@ -17,21 +17,22 @@ namespace lab_one
 
             string[] questions = //aray that holds questions 
             {
-                "this is a test question?","Which OS does .NET core NOT support, while being self stable?","?","?","Of the following, Wich are True?","?","?","?","?","?"
+                ".Net Core originated from which of the following companies?","Which OS does .NET core NOT support, while being self stable?"," When was .NET core 1.0 released?"," What is .Net Core written in?","Of the following, Wich are True about .Net Core?",
+                " What is the next big version of .Net Core to be released?"," Which of the following, is the True about .Net Core but NOT Framework?","Of the following which IDEs works best with .NET Core?"," which of the following is not an architecture?","Which of the following is .NET core not compatible?"
             };
 
             string[ , ] quizOptions = // multi dimensonal array that holds quiz questions
             {
-                { "A.) Apple","B.) Bannana","C.) Strawberry","D.) Orange"},
+                { "A.) Apple","B.) Microsoft","C.) Google","D.) Orange"},
                 { "A.) MacOS ","B.) SkyOS ","C.) Windows ","D.) Linux "},
-                { "A.) ","B.) ","C.) ","D.) "},
-                { "A.) ","B.) ","C.) ","D.) "},
+                { "A.) Jan 13, 1998","B.) Jan 2, 2007","C.) June 27, 2016","D.) Apr 27, 2017"},
+                { "A.) Both C# and C++","B.) C++ only","C.) C# only","D.) Python"},
                 { "A.) Free ","B.) Open source ","C.) Is a software framework ","D.) All the above"},
-                { "A.) ","B.) ","C.) ","D.) "},
-                { "A.) ","B.) ","C.) ","D.) "},
-                { "A.) ","B.) ","C.) ","D.) "},
-                { "A.) ","B.) ","C.) ","D.) "},
-                { "A.) ","B.) ","C.) ","D.) "}
+                { "A.) 3.0","B.) 1.3","C.) 2.2","D.) 4.0"},
+                { "A.) Cross-Platform","B.) .NET Core was made by Apple","C.) .Net Core runs worse than Framework","D.) .Net Core is unstable"},
+                { "A.) MonoDevelop","B.) Notepad++","C.) Visual Studio 2017","D.) Microsoft Word"},
+                { "A.) x86","B.) x32","C.) ARM","D.) False"},
+                { "A.) .Org","B.) Xamarin","C.) Mono","D.) .NET Framework"} //.NET Framework, Xamarin and Mono
 
             };
             string[] questionNum = {"1. ","2. ","3. ","4. ","5. ","6. ","7. ","8. ","9. ","10. "}; // use this as guide for the program
